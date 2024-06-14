526dabb4d656c6270e56bbc31eff5d1079fa8f7d
Mahmoud Mazouz <mazouz.mahmoud@outlook.com>
2024-06-14 17:34:26 +0200

    Add `vanilla_many_slow_queryables` bin

---
+++ src/bin/vanilla_many_slow_queryables.rs
@@ -0,0 +1,45 @@
+use std::{env, sync::Arc};
+
+use tokio::{task, time};
+use zenoh::prelude::r#async::*;
+
+const KEY_EXPR: &str = "slow_queryable";
+const VALUE: &str = "🐢";
+
+async fn declare_queryable(session: Arc<Session>, i: usize) {
+    let queryable = session.declare_queryable(KEY_EXPR).res().await.unwrap();
+
+    {
+        let queryable = queryable.clone();
+        task::spawn(async move {
+            let mut previous = queryable.len();
+            loop {
+                let current = queryable.len();
+                if current != previous {
+                    previous = current;
+                    eprintln!("slow_queryable: {i}: channel-len: {}", current);
+                }
+            }
+        });
+    }
+
+    while let Ok(query) = queryable.recv_async().await {
+        let reply = Ok(Sample::try_from(KEY_EXPR, VALUE).unwrap());
+        time::sleep(time::Duration::from_secs(1)).await;
+        query.reply(reply).res().await.unwrap();
+        eprintln!("slow_queryable: {i}: replied: {}", query);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    zenoh_util::try_init_log_from_env();
+
+    let config_path = env::args().nth(1).unwrap();
+    let config = Config::from_file(config_path).unwrap();
+    let session = Arc::new(zenoh::open(config).res().await.unwrap());
+
+    for i in 0..10 {
+        task::spawn(declare_queryable(session.clone(), i));
+    }
+}
